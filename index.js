fb888c251eae061ce17a36ef701ac8f2b2d0854d
Marina <95050756+Marindala@users.noreply.github.com>
2023-08-13 23:41:51 -0300

    limited requests!

--- index.js
+++ index.js
@@ -14,7 +14,20 @@ btn.addEventListener("click", async () => {
 
   btn.disabled = true;
 
+/*   const { Configuration, OpenAIApi } = require("openai");
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+const response = await openai.createImage({
+  prompt: "A cute baby sea otter",
+  n: 2,
+  size: "1024x1024",
+}); */
+
+
   const res = await fetch("https://api.openai.com/v1/images/generations", {
+    /* El error que estás viendo se refiere a un problema en tu código JavaScript que está intentando realizar una solicitud POST a la API de OpenAI para generar imágenes. El mensaje de error "Billing hard limit has been reached" indica que has alcanzado el límite de facturación establecido en la API de O */
     method: "POST",
     body: JSON.stringify({ /* convierte en string */
       prompt: input.value,
